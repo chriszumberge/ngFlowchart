cb457792cb868d6667f9e2ecd458c76abe287ff2
Christopher Zumberge <chriszumberge@gmail.com>
2017-12-26 10:34:47 -0500

    Added a very simple demo

---
+++ demo/data.js
@@ -0,0 +1,30 @@
+﻿var data = {
+    operators: {
+        operator: {
+            top: 20,
+            left: 20,
+            properties: {
+                title: 'Operator',
+                inputs: {
+                    input_1: {
+                        label: 'Input 1',
+                    },
+                    input_2: {
+                        label: 'Input 2'
+                    }
+                },
+                outputs: {
+                    output_1: {
+                        label: 'Output 1',
+                    },
+                    output_2: {
+                        label: 'Output 2',
+                    },
+                    output_3: {
+                        label: 'Output 3',
+                    }
+                }
+            }
+        }
+    }
+};
